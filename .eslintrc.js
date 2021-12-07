635dd2d025e6014127fd5030fcc3a25db4a72139
lfy <lifangyuan@123kanfang.com>
2021-12-07 09:14:03 +0800

    添加pritter、eslint、husky配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,26 @@
 module.exports = {
-  root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true
   },
-  'extends': [
+  extends: [
+    'airbnb-base',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'prettier'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    'no-console': 'off', //process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off', // process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': 'off', // process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "no-unused-vars": "off",
-    "no-undef": "off"
+    'no-unused-vars': 'off',
+    'no-undef': 'off'
   }
 }
